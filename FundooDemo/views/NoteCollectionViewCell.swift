4e6d3ee3f3551139a85245dc79c45e61bc17ddcc
BridgeLabz <bridgelabz@BridgeLabzs-MacBook-Pro.local>
2019-08-17 11:29:17 +0530

    new code added for grid view

---
+++ FundooDemo/views/NoteCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  NoteCollectionViewCell.swift
+//  FundooDemo
+//
+//  Created by BridgeLabz on 16/08/19.
+//  Copyright © 2019 BridgeLabz. All rights reserved.
+//
+
+import UIKit
+
+class NoteCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
